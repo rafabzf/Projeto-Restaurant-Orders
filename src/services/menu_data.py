86a22598fab92658a547fa5e984c70bb07728877
Rafael <rafinhabzf@gmail.com>
2023-07-10 14:33:01 -0300

    Req 3 e correção lint

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,32 @@
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path):
+        self.source_path = source_path
+        self.dish = set()
+        self._load_menu()
+
+    def _load_menu(self):
+        with open(self.source_path, newline='') as file:
+            csv_reader = csv.DictReader(file)
+            dish = {}
+
+            for row in csv_reader:
+                name_dish = row['dish']
+                name_ingredient = row['ingredient']
+                price = float(row['price'])
+                recipe_amount = int(row['recipe_amount'])
+
+                if name_dish not in dish:
+                    dish_uni = Dish(name_dish, price)
+                    dish[name_dish] = dish_uni
+                else:
+                    dish_uni = dish[name_dish]
+
+                ingredient = Ingredient(name_ingredient)
+                dish_uni.add_ingredient_dependency(ingredient, recipe_amount)
+        self.dish = set(dish.values())
